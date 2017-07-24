95e2d3602cfa801c53d9372af88158802eda4a8b
rm-rf-slant <kuronukoswing@gmail.com>
2017-07-25 02:39:22 +0900

    Add conditional branch

--- init.sh
+++ init.sh
@@ -7,4 +7,6 @@ readonly COMMAND="/usr/local/bin/brew"
 if [ ! -e ${COMMAND} ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     ${COMMAND} doctor && ${COMMAND} update && ${COMMAND} tap Homebrew/bundle && ${COMMAND} bundle
+else
+    echo "Initialization has already been completed."
 fi
